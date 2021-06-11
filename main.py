d6ab294357a889a44ff46c1eed1427c5ead73866
Josiah Lansford <josiahlansford@gmail.com>
2021-06-11 13:28:03 -0400

    Let's see if this works

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from git import Repo
 import time
 import sys
+from settings import REPO_NAME
+from slackbot import post_message
+
 
 def main(branch_name: str = "develop"):
   done = False
 
+  checkout(branch_name)
+
   while not done:
     update(branch_name)
-    time.sleep(10)
+    time.sleep(60*5)
+
+def checkout(branch_name: str):
+  repo = Repo(REPO_NAME)
+  repo.git.fetch()
+  print(repo.git.checkout(branch_name))
 
 def update(branch_name: str):
-  repo = Repo('C:/Users/jlansford/source/repos/max_shadow')
-  print(repo.git.checkout(branch_name))
+  repo = Repo(REPO_NAME)
   output = repo.git.pull()
-  if not output == 'Already up to date.':
+  if not 'Already up to date.' in output:
     print("updated!")
-  else:
-    print("no update")
-
+    commit = repo.commit('HEAD')
+    pr_name = commit.message.split('\n')[0]
+    print(output)
+    message = f'🎉 Congratulations to {commit.author} for merging {pr_name} to develop! 🎉' 
+    print(message)
+    post_message(message)
 if __name__ == "__main__":
   if (len(sys.argv) > 1):
     main(sys.argv[1])
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from slackbot import post_message
 
-response = post_message("Hello, World!")
+response = post_message("Hello, World! 🎉")
 print(response.status_code, response.text)
